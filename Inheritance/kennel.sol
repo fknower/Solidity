8cdd3434e95cd69ffcadefc40549271a1b437be4
Mr Francisco Knower <42910041+fknower@users.noreply.github.com>
2018-09-06 09:09:36 +0100

    Update kennel.sol

--- Inheritance/kennel.sol
+++ Inheritance/kennel.sol
@@ -3,9 +3,13 @@ import './dog.sol';
 contract Kennel is DogContract {
     function TransferDog(address _NewOwner) public returns (uint){
         address owner = msg.sender;
+        // error chech if owner is old owner
+        require(owner != _NewOwner);
         uint dogId = ownerToDog[owner];
         delete(ownerToDog[owner]);
         ownerToDog[_NewOwner]= dogId;
+        // error check that old owner has no dog
+        assert(ownerToDog[owner] == 0);
     }  
   function addKennelDog(string _name, uint _age) public{
         AddDog(_name, _age);
